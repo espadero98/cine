e50277cb02307af7b54a457123cdacfffca34048
farfanjaime <>
2018-06-21 07:59:01 -0600

    Agregando clase

---
+++ src/clases/Director.java
@@ -0,0 +1,27 @@
+
+package clases;
+
+public class Director {
+    
+    private String idDirector;
+    private String director;
+
+    public String getIdDirector() {
+        return idDirector;
+    }
+
+    public void setIdDirector(String idDirector) {
+        this.idDirector = idDirector;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+    
+    
+    
+}
---
+++ src/clases/Genero.java
@@ -0,0 +1,27 @@
+
+package clases;
+
+public class Genero {
+    
+    private String idGenero;
+    private String genero;
+
+    public String getIdGenero() {
+        return idGenero;
+    }
+
+    public void setIdGenero(String idGenero) {
+        this.idGenero = idGenero;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+    
+    
+    
+}
